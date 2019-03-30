67bd7cb49e2d7fcf34a192c4a1c6a16b8a2f2e7e
Ranu Pratap Singh <ranu.singh@unilodgers.com>
2019-03-30 18:08:23 +0530

    Updates

---
+++ precache-manifest.836e00beede4588a1fc7764c43c4195a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf0eb711c52dee4a65ba",
+    "url": "/transliteration_en_hi/static/css/main.37151c05.chunk.css"
+  },
+  {
+    "revision": "bf0eb711c52dee4a65ba",
+    "url": "/transliteration_en_hi/static/js/main.ba876fe2.chunk.js"
+  },
+  {
+    "revision": "3ed6be015e331fd82aca",
+    "url": "/transliteration_en_hi/static/js/runtime~main.1733df54.js"
+  },
+  {
+    "revision": "9fdc255086adcbbe80d8",
+    "url": "/transliteration_en_hi/static/js/2.ee5bb996.chunk.js"
+  },
+  {
+    "revision": "7bb0a9de061d5ed4ab7caeb9afae63c3",
+    "url": "/transliteration_en_hi/index.html"
+  }
+];
